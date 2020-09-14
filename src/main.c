b57114858f5ad758024bf7f0e1e7486768c3ac47
bbibentyo <bibenb1@users.noreply.github.com>
2020-09-13 23:25:24 -0400

    adding text to display names on board

--- src/main.c
+++ src/main.c
@@ -1,38 +1,59 @@
-#include <device.h>
-#include <drivers/display.h>
-#include <lvgl.h>
-#include <stdio.h>
-#include <string.h>
 #include <zephyr.h>
+#include <device.h>
+#include <display/cfb.h>
+#include <stdio.h>
 
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
-#include <logging/log.h>
-LOG_MODULE_REGISTER(app);
+#if defined(CONFIG_SSD16XX)
+#define DISPLAY_DRIVER	"SSD16XX"
+#endif
 
 void main(void)
 {
-	const struct device *display_dev;
-	lv_obj_t *display_label;
+	const struct device *dev;
+	int err;
 
-	display_dev = device_get_binding(CONFIG_LVGL_DISPLAY_DEV_NAME);
+	uint16_t rows;
+	uint8_t ppt;
+	uint8_t font_width;
+	uint8_t font_height;
+	uint8_t display_width;
+	uint8_t display_columns;
 
-	if (display_dev == NULL) {
-		LOG_ERR("device not found... aborting!");
+	dev = device_get_binding(DISPLAY_DRIVER);
+
+	if (dev == NULL) {
+		printf("device not found... aborting!\n");
 		return;
 	}
-	if (IS_ENABLED(CONFIG_LVGL_POINTER_KSCAN)) {
-		lv_obj_t *display_button;
 
-		display_button = lv_btn_create(lv_scr_act(), NULL);
-		lv_obj_align(display_button, NULL, LV_ALIGN_CENTER, 0, 0);
-		lv_btn_set_fit(display_button, LV_FIT_TIGHT);
-		display_label = lv_label_create(display_button, NULL);
-	} else {
-		display_label = lv_label_create(lv_scr_act(), NULL);
+	if (display_set_pixel_format(dev, PIXEL_FORMAT_MONO10) != 0) {
+		printf("Failed to set required pixel format\n");
+		return;
 	}
-	lv_label_set_text(display_label, "Hello Boss!!!");
-	lv_obj_align(display_label, NULL, LV_ALIGN_CENTER, 0, 0);
 
-	lv_task_handler();
-	display_blanking_off(display_dev);
+	printf("initialized %s\n", DISPLAY_DRIVER);
+
+	if (cfb_framebuffer_init(dev)) {
+		printf("Framebuffer initialization failed\n");
+		return;
+	}
+
+	cfb_framebuffer_clear(dev, true);
+	display_blanking_off(dev);
+
+	rows = cfb_get_display_parameter(dev, CFB_DISPLAY_ROWS);
+	ppt = cfb_get_display_parameter(dev, CFB_DISPLAY_PPT);
+	display_width = cfb_get_display_parameter(dev, CFB_DISPLAY_WIDTH);
+	display_columns = cfb_get_display_parameter(dev, CFB_DISPLAY_COLS);
+
+	printf("width: %d, columns: %d, rows: %d, ppt: %d",
+			display_width, display_columns, rows, ppt);
+
+	while(1) {
+		cfb_framebuffer_clear(dev, true);
+		cfb_print(dev, "Welcome Elikya", 0, 0);
+		cfb_print(dev, "Welcome Nayah", 0, 40);
+		cfb_framebuffer_finalize(dev);
+		k_sleep(K_SECONDS(15))false;
+	}
 }
